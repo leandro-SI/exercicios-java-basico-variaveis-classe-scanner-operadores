717d5e424559b23a0c0ceb4bd883a7e2f04d34e3
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-11-16 01:21:20 -0200

    Exercicio 1

---
+++ src/Exercicios/Exercicio1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicios;
+
+/**
+ * 1 - Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+ * @author Leandro
+ */
+public class Exercicio1 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Alo mundo");
+    }
+}
